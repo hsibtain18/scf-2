306ed598762d2bc458e8a3b1013f54c059a1f4ff
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2020-08-06 13:35:16 +0500

    financing

---
+++ src/app/admin/financing/financing-view/financing-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-financing-view',
+  templateUrl: './financing-view.component.html',
+  styleUrls: ['./financing-view.component.scss']
+})
+export class FinancingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
