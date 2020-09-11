e5d988caf7aa11be4356cd9558a2b2505157a711
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2020-09-11 16:47:16 +0500

    password

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -4,6 +4,7 @@ import { EncryptDecryptService } from 'src/app/Shared/services/encrypt-decrypt.s
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { loadingConfig } from 'src/app/const/config';
 
 @Component({
   selector: 'app-change-password',
@@ -12,7 +13,10 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ResetPasswordComponent implements OnInit {
   form: FormGroup;
-
+  passwordMismatch = false
+  private loginFormSubscriber: any
+  public showSpinner: boolean = false;
+  public spinnerConfig: any;
   constructor(private _encrypt: EncryptDecryptService,
     private _authService: AuthService,
     private _route: Router,
@@ -25,20 +29,39 @@ export class ResetPasswordComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.spinnerConfig = loadingConfig;
+    this.onChanges();
+  }
+  ngOnDestroy() {
+    this.loginFormSubscriber && this.loginFormSubscriber.unsubscribe();
   }
 
+  onChanges(): void {
+    this.loginFormSubscriber = this.form.valueChanges.subscribe(val => {
+      if (this.form.controls['Password'].value == this.form.controls['ConfirmPassword'].value) {
+        this.passwordMismatch = false;
+      }
+      else {
+        this.passwordMismatch = true
+      }
+    });
+  }
   ResetPassword() {
+
     if (this.form.valid && this.form.controls['Password'].value == this.form.controls['ConfirmPassword'].value) {
+      this.showSpinner = true;
       this._authService.PostCalls('login/ChangePassword', this.form.value)
         .then(val => {
           this.toast.success('Password Successfully Changed')
+          this.showSpinner = false;
         })
         .catch(err => {
+          this.showSpinner = false;
 
         })
     }
     else {
-
+      this.passwordMismatch = true
     }
   }
 
